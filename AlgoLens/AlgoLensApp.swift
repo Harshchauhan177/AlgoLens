e037755e0d9b0d91930cffd60e622ea8448b721c
Harshchauhan177 <harsh7617rajput@gmail.com>
2026-01-05 20:34:23 +0530

    initial commit

---
+++ AlgoLens/AlgoLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoLensApp.swift
+//  AlgoLens
+//
+//  Created by harsh chauhan on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AlgoLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgoLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlgoLens
+//
+//  Created by harsh chauhan on 05/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
